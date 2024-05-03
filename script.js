3a48901701e6138a5f1739cbc8b7b0484fb264bb
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-05-03 13:42:38 -0500

    second half progress

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const checkMessageButton = document.getElementById('check-message-btn');
 
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
-const freeRegex = /free money/i;
-const denyList = [helpRegex, dollarRegex];
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]\s/i;
+const denyList = [helpRegex, dollarRegex, freeRegex];
 
 const isSpam = msg => denyList.some(regex => regex.test(msg));
 
